23e1b31d570c82bd7bef108ce2c561abdec264be
lauris47 <lauris4747@gmail.com>
2016-11-06 18:23:03 +0100

    P5 - Mindful game

---
+++ Assets/Scripts/PlayAnimation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAnimation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+  
+        print("in");
+        GetComponent<Animator>().Play("Default");
+    }
+}
