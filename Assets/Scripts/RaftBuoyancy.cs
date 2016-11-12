631d19be45d7fe77db21bcaa35de87fa1e74a267
Mads\MadsKD <mads_did@hotmail.com>
2016-11-12 16:15:53 +0100

    Added raft buoyancy rotation and moved sound files

---
+++ Assets/Scripts/RaftBuoyancy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaftBuoyancy : MonoBehaviour {
+    public Transform target;
+    [SerializeField]
+    private float rotationMultiplier;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localRotation = Quaternion.Euler(new Vector3(transform.InverseTransformPoint(target.position).z, 0, -transform.InverseTransformPoint(target.position).x)*rotationMultiplier);
+	}
+}
