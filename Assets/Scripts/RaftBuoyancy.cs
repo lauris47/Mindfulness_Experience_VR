648db9596b130bcaba5eaa7a64cf9fc3ab0182ea
lauris47 <lauris4747@gmail.com>
2016-11-21 18:05:52 +0100

    Dynamic Reeds, Clean up.

--- Assets/Scripts/RaftBuoyancy.cs
+++ Assets/Scripts/RaftBuoyancy.cs
@@ -13,6 +13,10 @@ public class RaftBuoyancy : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.localRotation = Quaternion.Euler(new Vector3(transform.InverseTransformPoint(target.position).z, 0, -transform.InverseTransformPoint(target.position).x)*rotationMultiplier);
+
 	}
+    void LateUpdate()
+    {
+        transform.localRotation = Quaternion.Euler(new Vector3(transform.InverseTransformPoint(target.position).z, 0, -transform.InverseTransformPoint(target.position).x) * rotationMultiplier);
+    }
 }
