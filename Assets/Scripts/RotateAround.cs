eb1ce9086bc20f02e5148e7df2282be48020b04e
lauris47 <lauris4747@gmail.com>
2016-12-04 15:59:52 +0100

    Animation Curve, windmill, house colors

---
+++ Assets/Scripts/RotateAround.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAround : MonoBehaviour {
+    void LateUpdate(){
+        transform.Rotate(Vector3.back * Time.deltaTime * 10);
+    }
+}
