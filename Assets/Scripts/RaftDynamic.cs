3562f5ca3dbb06369c44a926850eab028cce0c69
lauris47 <lauris4747@gmail.com>
2016-12-05 16:39:55 +0100

    Scene 3
    Path cealred, painting terrain, grass added, moving canoe etc.

---
+++ Assets/Scripts/RaftDynamic.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaftDynamic : MonoBehaviour {
+
+    public float occilationSpeedX;
+    public float occilationSpeedY;
+    public float occilationSpeedZ;
+
+    void Start()
+    {
+        occilationSpeedZ = 0.02f;
+        transform.Rotate(0, 0, -2.45f);
+    }
+
+    void Update()
+    {
+        transform.Rotate((Mathf.Sin(Time.time) * occilationSpeedX), (Mathf.Sin(Time.time) * occilationSpeedY), (Mathf.Sin(Time.time) * occilationSpeedZ));
+    }
+}
